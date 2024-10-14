fffb8d7f1e932d513287acf09bebe0608bcb5f8b
dayoungdeanio <143829129+dayoungdeanio@users.noreply.github.com>
2024-10-14 10:38:23 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,27 +1,44 @@
 ############################################################
-# Name:
-# Name(s) of anyone worked with:
-# Est time spent (hr):
+# Name: D'Ante Dean
+# Est time spent (hr):Ican't remember.
 ############################################################
 
-from pgl import GWindow, GRect, GOval, GLine, GLabel
+from pgl import GWindow, GRect, GPolygon
 
-# Setting up initial window dimensions. 
-# You can change these if you want. They are in number of pixels.
 WIDTH = 600
-HEIGHT = 600
+HEIGHT = 400
+STRIPE_HEIGHT = HEIGHT // 3
 
-def draw_image():
-    """ Creates a window and draws a student's creation"""
-
-    # Creating the window
+def draw_bahamian_flag():
     gw = GWindow(WIDTH, HEIGHT)
+
+    # Aquamarine top 
+    top_stripe = GRect(0, 0, WIDTH, STRIPE_HEIGHT)
+    top_stripe.set_filled(True)
+    top_stripe.set_fill_color("#00CED1")
+    gw.add(top_stripe)
     
-    # And now it is your turn! Add your code below! Make sure you meet all the requirements!
+    # Gold 
+    middle_stripe = GRect(0, STRIPE_HEIGHT, WIDTH, STRIPE_HEIGHT)
+    middle_stripe.set_filled(True)
+    middle_stripe.set_fill_color("#FFD700")
+    gw.add(middle_stripe)
+    
+    # Aquamarine bottom 
+    bottom_stripe = GRect(0, 2 * STRIPE_HEIGHT, WIDTH, STRIPE_HEIGHT)
+    bottom_stripe.set_filled(True)
+    bottom_stripe.set_fill_color("#00CED1")
+    gw.add(bottom_stripe)
+    
+    # Black triangle
+    triangle = GPolygon()
+    triangle.add_vertex(0, 0)                      
+    triangle.add_vertex(0, HEIGHT)                 
+    triangle.add_vertex(WIDTH // 3, HEIGHT // 2)   
+    triangle.set_filled(True)
+    triangle.set_fill_color("black")
+    gw.add(triangle)
+    
+# Call the function to run
+draw_bahamian_flag()
 
-
-
-
-
-if __name__ == '__main__':
-    draw_image()
